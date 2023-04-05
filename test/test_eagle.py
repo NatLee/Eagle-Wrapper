1715a69a30b35d04dd05c38d237dc122ce307bfa
NatLee <natlee.work@gmail.com>
2023-04-06 02:32:33 +0800

    [fix] test func name

--- test/test_eagle.py
+++ test/test_eagle.py
@@ -3,11 +3,11 @@ from eaglewrapper import Eagle
 
 class TestGetImgInfoWithPath(unittest.TestCase):
 
-    def test_get_img_info_with_path(self):
+    def test_get_img_info_from_lib_path(self):
         eagle = Eagle()
         source_path = ''
         name_start_filters = ['example']
-        image_info = eagle.get_img_info_with_path(source_path, name_start_filters)
+        image_info = eagle.get_img_info_from_lib_path(source_path, name_start_filters)
         self.assertEqual(image_info, [])
 
 if __name__ == '__main__':
